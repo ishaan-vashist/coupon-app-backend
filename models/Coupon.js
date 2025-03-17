0dca978cb072d61ca791d8bb8d82a4c6b2ef7ea7
Ishaan Vashist <ishaanvashista@gmail.com>
2025-03-17 22:37:54 +0530

    Update Coupon.js

--- models/Coupon.js
+++ models/Coupon.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
-const CouponSchema = new mongoose.Schema({
-  code: { type: String, required: true, unique: true },
-  status: { type: String, enum: ["available", "claimed"], default: "available" },
-  assignedTo: { type: String, default: null }, // Stores IP or User ID
-  timestamp: { type: Date, default: Date.now }
-});
+const CouponSchema = new mongoose.Schema(
+  {
+    code: { type: String, required: true, unique: true },
+    status: { type: String, default: "available" },
+    assignedTo: { type: String, default: null },
+  },
+  { timestamps: true } //Ensure timestamps are enabled
+);
 
 module.exports = mongoose.model("Coupon", CouponSchema);
